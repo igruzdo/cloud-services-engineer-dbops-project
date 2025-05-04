2815ef8a48535da0d40933e7703b16efaedb95fb
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-05-04 19:10:49 +0530

    done

--- migrations/V004__create_index.sql
+++ migrations/V004__create_index.sql
@@ -1,5 +1,6 @@
 -- Создание индекса на столбец order_id в таблице order_product
 CREATE INDEX order_product_order_id_idx ON order_product (order_id);
 
+
 -- Создание составного индекса на столбцы status и date_created в таблице orders
 CREATE INDEX orders_status_date_idx ON orders (status, date_created);
