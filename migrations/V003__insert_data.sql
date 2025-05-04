19d7a5e6ec7a1eeb689763650024b4b2fcac158c
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-05-04 20:22:42 +0530

    done

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -9,12 +9,12 @@ FROM generate_series(1, 1000000) s(i);
 
 -- Добавление шести записей в таблицу product — список сосисок с ценами и ссылками на изображения
 INSERT INTO product (id, name, picture_url, price)
-VALUES (1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00),
-       (2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),
-       (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
-       (4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00),
-       (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
-       (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
+VALUES (273479, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00),
+       (982759, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),
+       (293849, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
+       (234559, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00),
+       (545669, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
+       (668799, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
 -- Заполнение таблицы order_product 1 000 000 строк, связывающих заказы с товарами
 -- Количество товара — случайное от 1 до 50
